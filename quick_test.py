badb9e0352d1661727bbc9780509f7745a66b33c
Justin Ely <justincely@gmail.com>
2015-07-06 14:27:59 -0500

    removing old files

--- quick_test.py
+++
@@ -1,3 +0,0 @@
-import REFSTIS_wrapper
-
-REFSTIS_wrapper.make_ref_files( 'darks_biases/2013_13134_17/' , clean=True)
--- quick_test_2.py
+++
@@ -1,4 +0,0 @@
-import REFSTIS_basejoint
-import glob
-
-REFSTIS_basejoint.make_basebias( glob.glob('test/*raw.fits') ) 

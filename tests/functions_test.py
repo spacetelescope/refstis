59c73619bf369432542e6827ab76c112c311edec
Sean Lockwood <lockwood@stsci.edu>
2026-08-14 12:15:30 -0400

    Added test for refaver.

--- tests/functions_test.py
+++ tests/functions_test.py
@@ -1,5 +1,8 @@
-from refstis import functions
+import os
+import shutil
 import numpy as np
+from astropy.io import fits
+from refstis import functions
 
 
 def test_residuals_image():
@@ -13,3 +16,28 @@ def test_residuals_image():
     sample_image = np.arange(20).repeat(20).reshape((20, 20))
     res_image = functions.make_resicols_image(sample_image)
     assert (res_image == np.ones((20, 20)) * 9.5).all(), 'Error in interesting array'
+
+
+def test_refaver(tmp_path, basedark):
+    """Tests refaver and call to msarith.
+    """
+    outfile = str(tmp_path / 'avg_dark.fits')
+
+    input_files = []
+    for i in range(1, 2 + 1):
+        input_file = os.path.join(tmp_path, f'basedark_{i:.0f}.fits')
+        input_files.append(input_file)
+        shutil.copy(str(basedark), input_file)
+
+    cwd = os.getcwd()
+    try:
+        os.chdir(tmp_path)
+        functions.refaver(input_files, combined_name=outfile)
+    finally:
+        os.chdir(cwd)
+
+    fits.info(outfile)
+
+    with fits.open(outfile) as f:
+        assert f[1].data[500, 500] == 0.
+        assert f[1].data[592, 593] == 10.

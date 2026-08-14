d159f68e45f7a6520e26c9b8d9933254743d794d
Sean Lockwood <lockwood@stsci.edu>
2026-08-14 10:23:04 -0400

    Moved unused code from functions.py out of production file.

--- tests/functions_test.py
+++ tests/functions_test.py
@@ -13,17 +13,3 @@ def test_residuals_image():
     sample_image = np.arange(20).repeat(20).reshape((20, 20))
     res_image = functions.make_resicols_image(sample_image)
     assert (res_image == np.ones((20, 20)) * 9.5).all(), 'Error in interesting array'
-
-
-def test_figure_number_of_periods():
-    pass
-
-
-def test_figure_days_in_period():
-
-    assert functions.figure_days_in_period(4, 28) == [7, 7, 7, 7], "Failure"
-    assert functions.figure_days_in_period(4, 29) == [8, 7, 7, 7], "Failure"
-
-
-def test_translate_date_string():
-    pass
